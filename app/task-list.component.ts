ced4076609590291a81b938f99363aedd8989e6a
L.Devin MacKrell <ldmackrell@gmail.com>
2017-10-26 12:42:54 -0700

    Update app to view tasks based on whether they are complete or not.

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,15 +4,26 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
-  <ul>
-    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}}  <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
-  </ul>
+    <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+    </select>
+
+    <ul>
+      <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+        <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+        <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+      <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
+      </li>
+    </ul>
   `
 })
 
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
+  filterByCompleteness: string = "incompleteTasks";
 
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
@@ -26,15 +37,23 @@ export class TaskListComponent {
     }
   }
 
-  priorityColor(currentTask) {
-    if (currentTask.priority === 3) {
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
   }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+
+  // priorityColor(currentTask) {
+  //   if (currentTask.priority === 3) {
+  //     return "bg-danger";
+  //   } else if (currentTask.priority === 2) {
+  //     return "bg-warning";
+  //   } else {
+  //     return "bg-info";
+  //   }
+  // }
 
 
 
